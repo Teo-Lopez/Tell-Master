47f24029817136bc9e3a1e6beef878750a074d3b
Teo <teodorolg130@gmail.com>
2020-06-08 18:14:07 +0200

    added gameService

---
+++ client/src/services/games.service.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+class gamesService {
+  constructor() {
+    this.baseURL = "http://localhost:5000/api/games/";
+    this.service = axios.create({ baseURL: this.baseURL });
+  }
+
+  getLastGames() {
+    return this.service
+      .get("last10")
+      .then((res) => res.data.algo)
+      .catch((err) => console.log(err));
+  }
+}
+
+export default gamesService;

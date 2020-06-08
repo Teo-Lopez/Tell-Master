24ff9c6ec3d72772b4c129906dd726bf5667f1c5
Teo <teodorolg130@gmail.com>
2020-06-08 18:13:24 +0200

    added lastGames component

---
+++ client/src/components/LastGames.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+
+function LastGames({ games }) {
+  return (
+    <div>
+      {games.map((game) => (
+        <h2>hey</h2>
+      ))}
+    </div>
+  );
+}
+
+export default LastGames;

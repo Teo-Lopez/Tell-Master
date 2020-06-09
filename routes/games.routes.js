033ed34c6312e9dd194f5d5813580a570e7bd9f8
Teo <teodorolg130@gmail.com>
2020-06-09 13:20:36 +0200

    added signup/login auth, without persistence

--- routes/games.routes.js
+++ routes/games.routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/last10", (req, res, next) => {
+router.get("/", (req, res, next) => {
+  
   res.json({ algo: ["hey", 5] });
+  //TO-DO Employ req.query.limit for DB search.
 });
 
 module.exports = router;

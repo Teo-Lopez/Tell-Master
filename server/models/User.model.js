fc067776ada9ae62c64aa7fb7db2540417632927
Teo <teodorolg130@gmail.com>
2020-06-11 12:35:55 +0200

    removed savedGame property from model

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: { type: String, required: true },
+  username: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   savedGames: { type: [mongoose.SchemaTypes.ObjectId], default: [] },

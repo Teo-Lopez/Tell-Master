681c218f9c67a671a15fd0bbf0c783006b8186da
Teo <teodorolg130@gmail.com>
2020-06-12 13:36:22 +0200

    added service for login and signup

---
+++ client/src/services/auth.service.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+class authService {
+  constructor() {
+    this.baseURL = "http://localhost:5000/api/auth/";
+    this.service = axios.create({ baseURL: this.baseURL, withCredentials: true });
+  }
+
+  login({ username, email, password }) {
+    return this.service
+      .post("login", { username, email, password }) //add form
+      .then((res) => res.data)
+      .catch((err) => console.log({ err }));
+  }
+
+  signup({ username, email, password }) {
+    return this.service
+      .post("signup", { username, email, password }) //add form
+      .then((res) => res.data)
+      .catch((err) => console.log({ err }));
+  }
+
+  logout() {
+    return this.service
+      .post("logout")
+      .then((res) => res.data)
+      .catch((err) => console.log({ err }));
+  }
+
+  loggedin() {
+    return this.service
+      .get("loggedin")
+      .then((res) => res.data)
+      .catch((err) => console.log({ err }));
+  }
+}
+
+export default authService;

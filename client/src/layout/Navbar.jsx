50750e2806a8aa6de5d49ae9f345090d6ac25231
Teo <teodorolg130@gmail.com>
2020-06-12 13:48:38 +0200

    created links in navbar and differentiated logged navbar from guest nav

--- client/src/layout/Navbar.jsx
+++ client/src/layout/Navbar.jsx
@@ -7,63 +7,64 @@ function Navbar({ loggedInUser, showLogin, showSignup }) {
     <Spinner />
   ) : loggedInUser === null ? (
     <Container fluid>
-      <Nav variant="pills">
-        <Row className="justify-content-between" style={{ width: "100vw" }}>
-          <Col lg={5} style={{ display: "flex" }}>
-            <Nav.Item>
-              <Nav.Link>Mis partidas</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link>Mis personajes</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link>Crear Partida</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link onClick={showLogin}>Login</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link onClick={showSignup}>Signup</Nav.Link>
-            </Nav.Item>
-          </Col>
-          <Col lg={2}>
-            <NavDropdown title="Mi perfil" id="nav-dropdown" drop="left">
-              <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>
-              <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item eventKey="4.3">Something else here</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Col>
-        </Row>
-      </Nav>
+      <div className="navbar">
+        <Nav variant="pills">
+          <Row className="justify-content-between" style={{ width: "100vw" }}>
+            <Col lg={5} style={{ display: "flex" }}>
+              <Nav.Item>
+                <Nav.Link onClick={showLogin}>Login</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link onClick={showSignup}>Signup</Nav.Link>
+              </Nav.Item>
+            </Col>
+            <Col lg={2}>
+              <NavDropdown title="Mi perfil" id="nav-dropdown" drop="left">
+                <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>
+                <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item eventKey="4.3">Something else here</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Col>
+          </Row>
+        </Nav>
+      </div>
     </Container>
   ) : (
     <Container fluid>
-      <Nav variant="pills">
-        <Row className="justify-content-between" style={{ width: "100vw" }}>
-          <Col lg={5} style={{ display: "flex" }}>
-            <Nav.Item>
-              <Nav.Link>Mis partidas</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link>Mis personajes</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link>Crear Partida</Nav.Link>
-            </Nav.Item>
-          </Col>
-          <Col lg={2}>
-            <NavDropdown title="Mi perfil" id="nav-dropdown" drop="left">
-              <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>
-              <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item eventKey="4.3">Something else here</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Col>
-        </Row>
-      </Nav>
+      <div className="navbar">
+        <Nav variant="pills">
+          <Row className="justify-content-between" style={{ width: "100vw" }}>
+            <Col lg={5} style={{ display: "flex" }}>
+              <Nav.Item>
+                <Nav.Link as={NavLink} to={"/myGames"}>
+                  Mis Partidas
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link as={NavLink} to={"/myCharacters"}>
+                  Mis personajes
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link as={NavLink} to={"/newGame"}>
+                  Crear Partida
+                </Nav.Link>
+              </Nav.Item>
+            </Col>
+            <Col lg={2}>
+              <NavDropdown title="Mi perfil" id="nav-dropdown" drop="left">
+                <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>
+                <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item eventKey="4.3">Something else here</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Col>
+          </Row>
+        </Nav>
+      </div>
     </Container>
   );
 }

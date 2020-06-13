4b0913f92fb830c33e91418ead210c928adb94cf
Teo <teodorolg130@gmail.com>
2020-06-13 14:35:13 +0200

    added timestamps to models

--- server/models/Game.model.js
+++ server/models/Game.model.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const gameSchema = new Schema({
-  title: String,
-  minLevel: Number,
-  description: String,
-  chapters: [mongoose.SchemaTypes.ObjectId],
-});
+const gameSchema = new Schema(
+  {
+    creator: mongoose.SchemaTypes.ObjectId,
+    title: String,
+    minLevel: Number,
+    description: String,
+    chapters: [mongoose.SchemaTypes.ObjectId],
+  },
+  { timestamps: true }
+);
 
 const Game = mongoose.model("Game", gameSchema);
 

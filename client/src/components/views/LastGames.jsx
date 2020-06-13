fcc133a326a7a6edd0114488a11e69fd0850aedd
Teo <teodorolg130@gmail.com>
2020-06-13 14:34:17 +0200

    now showing real last games

--- client/src/components/views/LastGames.jsx
+++ client/src/components/views/LastGames.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from "react";
 
 function LastGames({ games }) {
-  return (
-    <div>
-      {games.map((game) => (
-        <h2>hey</h2>
-      ))}
+  return games.map((game) => (
+    <div className="centered">
+      <h3>{game.title}</h3>
+      <em>Nivel minimo: {game.minLevel}</em>
+      <p>{game.description.slice(0, 100)}</p>
     </div>
-  );
+  ));
 }
 
 export default LastGames;

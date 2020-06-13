dc443717cef2bf20bafd84b4899a07bddf1e3576
Teo <teodorolg130@gmail.com>
2020-06-13 14:34:45 +0200

    restricted routes and superminor additions in css

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import authService from "./services/auth.service";
 import LoginForm from "./components/LoginForm";
 import SignupForm from "./components/SignupForm";
 import CenteredModal from "./components/Modal";
-
+import NewGame from "./components/views/NewGame";
 function App() {
   const GamesService = new gamesService();
   const AuthService = new authService();
@@ -21,15 +21,18 @@ function App() {
   function setUser(user) {
     setloggedInUser(user);
   }
-  //Recover games on mount
-  useEffect(() => {
+
+  function updateLastGames() {
     GamesService.getLastGames().then((last10) => {
-      console.log(!last10.every((elm) => lastGames.find((game) => game._id == elm._id)));
       if (Array.isArray(last10) && !last10.every((elm) => lastGames.find((game) => game._id == elm._id))) {
         setlastGames(last10);
       }
     });
+  }
 
+  //Recover games on mount
+  useEffect(() => {
+    updateLastGames();
     return () => {
       setlastGames([]);
     };
@@ -51,16 +54,22 @@ function App() {
     <Switch>
       <>
         <Navbar showSignup={() => setSignupModal(true)} showLogin={() => setloginModal(true)} loggedInUser={loggedInUser} />
-        <CenteredModal title={"Login"} show={loginModal} onHide={() => setloginModal(false)}>
-          <LoginForm setUser={setUser} />
-        </CenteredModal>
-        <CenteredModal title={"Signup"} show={signupModal} onHide={() => setSignupModal(false)}>
-          <SignupForm setUser={setUser} />
-        </CenteredModal>
         <Route exact path="/" render={() => <LastGames games={lastGames} />} />
-        {/* <Route exact path="/login" render={() => <LoginForm setUser={setUser} />} /> */}
-        <Route exact path="/signup" render={() => <SignupForm setUser={setUser} />} />
-        <Route exact path="/myGames" render={() => <MyGames loggedInUser={loggedInUser} />} />
+        {loggedInUser ? (
+          <>
+            <Route exact path="/myGames" render={() => <MyGames loggedInUser={loggedInUser} />} />
+            <Route exact path="/newGame" render={() => <NewGame updateLastGames={updateLastGames} loggedInUser={loggedInUser} />} />
+          </>
+        ) : (
+          <>
+            <CenteredModal title={"Login"} show={loginModal} onHide={() => setloginModal(false)}>
+              <LoginForm setUser={setUser} />
+            </CenteredModal>
+            <CenteredModal title={"Signup"} show={signupModal} onHide={() => setSignupModal(false)}>
+              <SignupForm setUser={setUser} />
+            </CenteredModal>
+          </>
+        )}
       </>
     </Switch>
   );

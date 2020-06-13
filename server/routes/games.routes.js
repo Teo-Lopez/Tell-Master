b308dc9aa8190056494afce98dbafdfc36335373
Teo <teodorolg130@gmail.com>
2020-06-13 14:35:34 +0200

    finished lastGames and createGame routes

--- server/routes/games.routes.js
+++ server/routes/games.routes.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const Game = require("../models/Game.model");
 
 router.get("/", (req, res, next) => {
-  res.json({ games: [{ _id: 1 }, { _id: 2 }] });
+  const today = new Date();
+  const lastWeek = new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000);
+  const limit = parseInt(req.query.limit) || 10;
+
+  Game.find({ createdAt: { $gte: lastWeek, $lt: today } })
+    .limit(limit)
+    .then((gamesFound) => res.json({ gamesFound }))
+    .catch((err) => res.json({ err }));
   //TO-DO Employ req.query.limit for DB search.
+});
+
+router.post("/", (req, res) => {
+  const { creator, title, minLevel, description } = req.body;
+  Game.create({ creator, title, minLevel, description })
+    .then((createdGame) => res.json(createdGame))
+    .catch((err) => res.json({ err }));
 });
 
 module.exports = router;

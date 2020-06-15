7e6b15913d8c191ca1b0354d5c9a5f834b8fb070
Teo <teodorolg130@gmail.com>
2020-06-15 17:50:28 +0200

    added key to LastGames map

--- client/src/components/views/LastGames.jsx
+++ client/src/components/views/LastGames.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from "react";
-
-function LastGames({ games }) {
-  return games.map((game) => (
-    <div className="centered">
-      <h3>{game.title}</h3>
+import { Link } from "react-router-dom";
+function LastGames({ games, loggedInUser }) {
+  return games.map((game, idx) => (
+    <div className="centered" key={idx}>
+      <h3>
+        <Link to={loggedInUser && loggedInUser._id === game.creator ? `/modify/${game._id}` : `/${game._id}`}>{game.title}</Link>
+      </h3>
       <em>Nivel minimo: {game.minLevel}</em>
-      <p>{game.description.slice(0, 100)}</p>
+      <p>{game.description.slice(0, 350)}</p>
     </div>
   ));
 }

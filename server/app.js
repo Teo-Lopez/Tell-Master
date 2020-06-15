aba96150ced96ee0e97d20cab91768cb4497faf0
Teo <teodorolg130@gmail.com>
2020-06-15 17:50:49 +0200

    added routing for choices and chapters

--- server/app.js
+++ server/app.js
@@ -12,5 +12,7 @@ const index = require("./routes/index");
 app.use("/", index);
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/games", require("./routes/games.routes"));
+app.use("/api/chapters", require("./routes/chapter.routes"));
+app.use("/api/choices", require("./routes/choices.routes"));
 
 module.exports = app;

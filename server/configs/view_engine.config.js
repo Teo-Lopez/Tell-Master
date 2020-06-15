568e865bd1536107ef381a0d1efa57272278b3f6
Teo <teodorolg130@gmail.com>
2020-06-15 18:34:52 +0200

    fixed path in viewengine

--- server/configs/view_engine.config.js
+++ server/configs/view_engine.config.js
@@ -6,7 +6,7 @@ const express = require("express");
 const favicon = require("serve-favicon");
 
 const view_engine = (app) => {
-  app.set("views", path.join(__dirname, "views"));
+  app.set("views", path.join(__dirname, "../views"));
   app.set("view engine", "hbs");
   app.use(express.static(path.join(__dirname, "public")));
   app.use(favicon(path.join(__dirname, "../public", "images", "favicon.ico")));

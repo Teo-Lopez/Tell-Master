aa8c1adce96a6adbc70dea3b2e23ddcd0ec61431
Teo <teodorolg130@gmail.com>
2020-06-19 11:49:04 +0200

    added timeout to the request

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -3,35 +3,52 @@ import axios from "axios";
 class authService {
   constructor() {
     this.baseURL = `${process.env.REACT_APP_API}/auth/`;
-    this.service = axios.create({ baseURL: this.baseURL, withCredentials: true });
+    this.service = axios.create({
+      baseURL: this.baseURL,
+      withCredentials: true,
+      timeout: 1000 * 30,
+      timeoutErrorMessage: "El servidor ha tardado demasiado tiempo en responder",
+    });
   }
 
   login({ username, email, password }) {
     return this.service
       .post("login", { username, email, password }) //add form
       .then((res) => res.data)
-      .catch((err) => console.log({ err }));
+      .catch((err) => {
+        console.log({ err });
+        if (err.code === "ECONNABORTED") console.error(err.message);
+      });
   }
 
   signup({ username, email, password }) {
     return this.service
       .post("signup", { username, email, password }) //add form
       .then((res) => res.data)
-      .catch((err) => console.log({ err }));
+      .catch((err) => {
+        console.log({ err });
+        if (err.code === "ECONNABORTED") console.error(err.message);
+      });
   }
 
   logout() {
     return this.service
       .post("logout")
       .then((res) => res.data)
-      .catch((err) => console.log({ err }));
+      .catch((err) => {
+        console.log({ err });
+        if (err.code === "ECONNABORTED") console.error(err.message);
+      });
   }
 
   loggedin() {
     return this.service
       .get("loggedin")
       .then((res) => res.data)
-      .catch((err) => console.log({ err }));
+      .catch((err) => {
+        console.log({ err });
+        if (err.code === "ECONNABORTED") console.error(err.message);
+      });
   }
 }
 

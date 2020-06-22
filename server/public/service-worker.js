119a22fabef3fd5d1dccc1688d7d09ae21760e3f
Teo <teodorolg130@gmail.com>
2020-06-22 21:23:53 +0200

    deploy

--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7f2533225a1b92ea5fbf198b585c95f3.js"
+  "/precache-manifest.c3883e0a7172e2f8240755709a9a6470.js"
 );
 
 self.addEventListener('message', (event) => {

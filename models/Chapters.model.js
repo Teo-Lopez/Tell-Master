e80723e519d414da0dd8acb6da0058925601cce1
Teo <teodorolg130@gmail.com>
2020-06-23 04:22:37 +0200

    added title to chapter model/routes/services and changed logo and some styles

--- models/Chapters.model.js
+++ models/Chapters.model.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const chapterSchema = new Schema(
-  {
+  { 
+    title: {type: String, required: true},
     description: String,
     choices: [{ type: mongoose.SchemaTypes.ObjectId, ref: "Choice" }],
   },

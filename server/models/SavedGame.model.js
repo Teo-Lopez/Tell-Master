67af97b72ad152c2488eeb2096f0fc7d89239d90
Teo <teodorolg130@gmail.com>
2020-07-08 00:11:27 +0200

    fixed reference in model

--- server/models/SavedGame.model.js
+++ server/models/SavedGame.model.js
@@ -5,7 +5,7 @@ const savedGameSchema = new Schema(
   {
     gameId: { type: mongoose.SchemaTypes.ObjectId, ref: "Game" },
     currentChapter: { type: mongoose.SchemaTypes.ObjectId, ref: "Chapter" },
-    choicesTree: [{ type: mongoose.SchemaTypes.ObjectId, ref: "Choice" }],
+    choicesTree: [{ type: mongoose.SchemaTypes.ObjectId, ref: "CharacterChoice" }],
     character: { type: mongoose.SchemaTypes.ObjectId, ref: "Character" },
     finished: { type: Boolean, required: true, default: false },
   },

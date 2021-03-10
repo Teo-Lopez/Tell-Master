180bc94ec85c8dc9b492ac46af40d38c937dd772
Teo <emaildeteo@gmail.com>
2021-03-10 19:29:47 +0100

    reworked simple char creation and cleaned component

--- client/src/components/pages/GameDetails/SimpleStartPlaying.jsx
+++ client/src/components/pages/GameDetails/SimpleStartPlaying.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { Dropdown } from 'react-bootstrap'
 import { Button } from '../../shared/Buttons'
@@ -6,18 +6,19 @@ import savedGamesService from '../../../services/savedGames.service'
 import chapterService from '../../../services/chapter.service'
 import characterService from '../../../services/character.service'
 import { withRouter } from 'react-router-dom'
+import UserContext from '../../../UserContext'
 const CharacterService = new characterService()
 const SavedGamesService = new savedGamesService()
 const ChapterService = new chapterService()
 
 function SimpleStartPlaying(props) {
-	const { game, savedGames, loggedInUser, setUser, updateSavedGames, history, match } = props
+	const { loggedInUser, setUser } = useContext(UserContext)
+	const { game, savedGames, updateSavedGames, history, match } = props
 
 	function createCharAndSave() {
-		const name = `Simple Game ${savedGames.length + 1}`
-
 		CharacterService.createSimpleCharacter().then(char => {
-			return CharacterService.assignCharacterToUser(loggedInUser._id, char._id).then(_ => newSave(char._id))
+			setUser()
+			newSave(char._id)
 		})
 	}
 	function newSave(chosenCharacter) {
@@ -54,45 +55,45 @@ function SimpleStartPlaying(props) {
 			createAndAssignCharacter
 		</section>
 	) : (
-				<section>
-					<article>
-						<h1>{game.title}</h1>
-						<p>{game.description}</p>
+		<section>
+			<article>
+				<h1>{game.title}</h1>
+				<p>{game.description}</p>
 
-						{loggedInUser ? (
-							game.chapters.length === 0 ? (
-								<Button text='Aún no tiene ningún capitulo' disabled={true} onClick={startGame} />
-							) : savedGames.length === 0 ? (
-								<Button text='Comienza la partida' onClick={startGame} />
-							) : (
-										<>
-											<Dropdown>
-												<Dropdown.Toggle variant='transparent' id='dropdown-basic'>
-													<Button text='Continua la aventura' />
-												</Dropdown.Toggle>
+				{loggedInUser ? (
+					game.chapters.length === 0 ? (
+						<Button text='Aún no tiene ningún capitulo' disabled={true} onClick={startGame} />
+					) : savedGames.length === 0 ? (
+						<Button text='Comienza la partida' onClick={startGame} />
+					) : (
+						<>
+							<Dropdown>
+								<Dropdown.Toggle variant='transparent' id='dropdown-basic'>
+									<Button text='Continua la aventura' />
+								</Dropdown.Toggle>
 
-												<Dropdown.Menu style={{ backgroundColor: 'rgba(40,40,40)' }} variant='transparent'>
-													{savedGames.map(save => (
-														<Link to={`/chapter/${save._id}`}>
-															<Dropdown.Item as={'div'}>
-																<Button text={save.character.name} />
-															</Dropdown.Item>
-														</Link>
-													))}
-													<Dropdown.Divider />
-													<Dropdown.Item as={'div'}>
-														<Button text={'Comienza una nueva aventura'} onClick={startGame}></Button>
-													</Dropdown.Item>
-												</Dropdown.Menu>
-											</Dropdown>
-										</>
-									)
-						) : (
-								<Button text='Continuar la aventura' disabled={true}></Button>
-							)}
-					</article>
-				</section>
-			)
+								<Dropdown.Menu style={{ backgroundColor: 'rgba(40,40,40)' }} variant='transparent'>
+									{savedGames.map(save => (
+										<Link to={`/chapter/${save._id}`}>
+											<Dropdown.Item as={'div'}>
+												<Button text={save.character.name} />
+											</Dropdown.Item>
+										</Link>
+									))}
+									<Dropdown.Divider />
+									<Dropdown.Item as={'div'}>
+										<Button text={'Comienza una nueva aventura'} onClick={startGame}></Button>
+									</Dropdown.Item>
+								</Dropdown.Menu>
+							</Dropdown>
+						</>
+					)
+				) : (
+					<Button text='Continuar la aventura' disabled={true}></Button>
+				)}
+			</article>
+		</section>
+	)
 }
 
 export default withRouter(SimpleStartPlaying)

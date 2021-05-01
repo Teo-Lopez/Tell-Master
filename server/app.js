bae913f12bec82f4a3bb65ce6d96088faba2fdb4
Teo <emaildeteo@gmail.com>
2021-05-01 17:54:31 +0200

    decoupled routes

--- server/app.js
+++ server/app.js
@@ -1,24 +1,16 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-require("./configs/mongoose.config");
-require("./configs/debug.config");
-require("./configs/middleware.config")(app);
-require("./configs/view_engine.config")(app);
-require("./configs/locals.config")(app);
-require("./configs/session.config")(app);
+require('dotenv').config()
+const express = require('express')
+const app = express()
+require('./configs/mongoose.config')
+require('./configs/debug.config')
+require('./configs/middleware.config')(app)
+require('./configs/view_engine.config')(app)
+require('./configs/locals.config')(app)
+require('./configs/session.config')(app)
 
-const index = require("./routes/index");
-app.use("/", index);
-app.use("/api/auth", require("./routes/auth.routes"));
-app.use("/api/games", require("./routes/games.routes"));
-app.use("/api/chapters", require("./routes/chapter.routes"));
-app.use("/api/choices", require("./routes/choices.routes"));
-app.use("/api/savedGames", require("./routes/savedGames.routes"));
-app.use("/api/characters", require("./routes/character.routes"));
-app.use("/api/characterChoices", require("./routes/characterChoices.routes"));
+require('./routes')(app)
 
 app.use((req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
-module.exports = app;
+	res.sendFile(__dirname + '/public/index.html')
+})
+module.exports = app
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const router  = express.Router();
-
-/* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
-});
-
-module.exports = router;
+module.exports = app => {
+	// Base URLS
+	app.use('/', require('./base.routes.js'))
+	app.use('/api/auth', require('./auth.routes'))
+	app.use('/api/games', require('./games.routes'))
+	app.use('/api/chapters', require('./chapter.routes'))
+	app.use('/api/choices', require('./choices.routes'))
+	app.use('/api/savedGames', require('./savedGames.routes'))
+	app.use('/api/characters', require('./character.routes'))
+	app.use('/api/characterChoices', require('./characterChoices.routes'))
+}

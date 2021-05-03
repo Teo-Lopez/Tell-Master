283c2c68814bdeeb7c9fd629ee37831f24f6256e
Teo <emaildeteo@gmail.com>
2021-05-03 10:41:42 +0200

    checklogged in post new character

--- server/routes/character.routes.js
+++ server/routes/character.routes.js
@@ -1,64 +1,53 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
-const Game = require("../models/Game.model")
-const Choice = require("../models/Choice.model")
-const Chapter = require("../models/Chapters.model")
-const Character = require("../models/Character.model")
-const User = require("../models/User.model")
+const Game = require('../models/Game.model')
+const Choice = require('../models/Choice.model')
+const Chapter = require('../models/Chapters.model')
+const Character = require('../models/Character.model')
+const User = require('../models/User.model')
+const { checkLoggedIn } = require('../middlewares')
 
-router.get("/user", (req, res, next) => {
-  const _id = req.query.chapterId || ""
+router.get('/user', (req, res, next) => {
+	const _id = req.query.chapterId || ''
 
-  User.findById(_id)
-    .lean()
-    .select({ characters: true })
-    .then((characters) => {
-      res.json({ characters })
-    })
+	User.findById(_id)
+		.lean()
+		.select({ characters: true })
+		.then(characters => {
+			res.json({ characters })
+		})
 })
 
-router.post("/", (req, res) => {
-  const newCharacter = req.body.character
-  newCharacter.maxhp = newCharacter.hp
-  newCharacter.level = 1
-  newCharacter.px = 0
-
-  Character.create(newCharacter)
-    .then((createdCharacter) => {
-      res.json(createdCharacter)
-    })
-    .catch((err) => res.json({ err }))
+router.post('/', checkLoggedIn, (req, res) => {
+	const { newCharacter } = req.body
+	console.log(newCharacter)
+	Character.createCharacter(newCharacter, req.session.user)
+		.then(createdCharacter => {
+			res.json(createdCharacter)
+		})
+		.catch(err => res.json(err))
 })
 
-router.post("/simple", (req, res) => Character.createSimpleCharacter(req.user._id)
-  .then((createdCharacter) => res.json(createdCharacter))
-  .catch((err) => res.json({ err }))
+router.post('/simple', (req, res) =>
+	Character.createSimpleCharacter(req.user._id)
+		.then(createdCharacter => res.json(createdCharacter))
+		.catch(err => res.json({ err }))
 )
 
-router.post("/assign", (req, res) => {
-  const { userId, characterId } = req.body
-
-  User.findByIdAndUpdate(userId, { $push: { characters: characterId } }, { new: true })
-    .lean()
-    .populate("characters")
-    .populate("savedGames")
-    .then((updatedUser) => {
-      res.json(updatedUser)
-    })
-    .catch((err) => res.json({ err }))
+router.patch('/', (req, res) => {
+	const { charId, newCharacter } = req.body
+	Character.findByIdAndUpdate(charId, newCharacter, { new: true })
+		.then(char => res.json(char))
+		.catch(err => res.status(401).json(err))
 })
 
-router.patch("/", (req, res) => {
-  //TO-DO UPDATE Char
-})
+router.delete('/', (req, res) => {
+	const { charId } = req.body
 
-router.delete("/", (req, res) => {
-  const { charId } = req.body
-
-  Character.findByIdAndDelete(charId)
-    .then((deletedCharacter) => {
-      res.json(deletedCharacter)
-    })
-    .catch((err) => res.json({ err }))
+	Character.findByIdAndDelete(charId)
+		.then(deletedCharacter => {
+			res.json(deletedCharacter)
+		})
+		.catch(err => res.json({ err }))
 })
 module.exports = router

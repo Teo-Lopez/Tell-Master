db0c531dae10f3736ccacca9f8f9a2b1e11dd9d6
Teo <emaildeteo@gmail.com>
2021-05-10 13:27:41 +0200

    fixed logout, improved login, cleaned up

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -1,17 +1,18 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const { toLower } = require('../utils')
+const Schema = mongoose.Schema
 
 const userSchema = new Schema(
-  {
-    username: { type: String, required: true },
-    email: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
-    savedGames: { type: [{ type: mongoose.SchemaTypes.ObjectId, ref: "SavedGame" }], default: [] },
-    characters: { type: [{ type: mongoose.SchemaTypes.ObjectId, ref: "Character" }], default: [] },
-  },
-  { timestamps: true }
-);
+	{
+		username: { type: String, required: true, set: name => toLower(name) },
+		email: { type: String, unique: true, required: true, set: mail => toLower(mail) },
+		password: { type: String, required: true },
+		savedGames: { type: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'SavedGame' }], default: [] },
+		characters: { type: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Character' }], default: [] }
+	},
+	{ timestamps: true }
+)
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema)
 
-module.exports = User;
+module.exports = User
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -2,14 +2,21 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User.model')
 const bcrypt = require('bcrypt')
-const passport = require('passport')
+
 //api/auth
 router.get('/', (req, res, next) => {
 	res.json({ user: { name: 'whatever' } })
 })
 
 router.post('/login', (req, res, next) => {
-	const { username, password } = req.body
+	let { username, password } = req.body
+
+	if (!username || !password) {
+		res.json({ message: 'Error. Comprueba que tus datos sean correctos.' })
+		return
+	}
+
+	username = username.toLowerCase()
 	User.findOne({ username })
 		.lean()
 		.populate('characters')
@@ -32,8 +39,8 @@ router.post('/login', (req, res, next) => {
 })
 
 router.post('/logout', (req, res, next) => {
-	// req.logout() is defined by passport
-	req.logout()
+	req.session.destroy()
+	req.session.user = null
 	res.status(200).json({ message: 'Log out success!' })
 })
 

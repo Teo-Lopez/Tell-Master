fd9543cfaad2b713ccaa9a207efbabb080801a99
Teo <emaildeteo@gmail.com>
2021-05-10 13:26:11 +0200

    added lowercase util

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
 module.exports = {
-	checkMongoId: id => mongoose.Types.ObjectId.isValid(id)
+	checkMongoId: id => mongoose.Types.ObjectId.isValid(id),
+	toLower: text => text.toLowerCase()
 }
